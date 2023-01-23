20be63864721e0421a238703bc1abdf8dd7a3950
deanjohns <srimal.git@gmail.com>
2023-01-23 17:17:27 +0530

    fixed

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,9 +1,22 @@
 import AppFunctionInput from "./AppFunctionInput";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef, createRef } from "react";
+import { useScreenshot } from "use-react-screenshot";
+import { jsPDF } from "jspdf";
 
 const AppFunction = () => {
 
     const [name, setName] = useState("");
+
+    const ref = createRef(null);
+    const [image, takeScreenShot] = useScreenshot();
+
+    const download = () => {
+        // var pdf = new jsPDF({ orientation: "landscape" });
+        // pdf.addImage(image, 'JPEG', 0, 0);
+        // pdf.save("download.pdf");
+        takeScreenShot(ref.current);
+
+    }
 
     const changeName = (e) => {
         const value = e.target.value;
@@ -14,10 +27,21 @@ const AppFunction = () => {
         setName("My default Function name");
     }, [])
 
+    // useEffect(() => {
+    //     takeScreenShot(ref.current);
+    // }, [])
+
+    useEffect(() => {
+        console.log("image", image);
+    }, [image])
+
     return (
-        <div className='app_main'>
+        <div className='app_main' ref={ref}>
             <h1 className='app_header'>{name}</h1>
             <AppFunctionInput name={name} changeName={changeName} />
+            <button onClick={download}>Download</button>
+            <br />
+            <img width="1300" src={image} alt={"ScreenShot"} />
         </div>
     )
 }
